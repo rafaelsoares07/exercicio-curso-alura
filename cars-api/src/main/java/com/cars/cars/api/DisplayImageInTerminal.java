0f06c8e7d94af9bf66bc72dc78e14de117b33f85
RafaelSoaresSMP <rafael.soares@simplesmeiosdepagamento.com.br>
2024-04-30 14:52:54 -0300

    feat: commit

--- cars-api/src/main/java/com/cars/cars/api/DisplayImageInTerminal.java
+++ cars-api/src/main/java/com/cars/cars/api/DisplayImageInTerminal.java
@@ -2,25 +2,70 @@ package com.cars.cars.api;
 
 import models.Baralho;
 import models.Carta;
+import models.DataBrandCar;
 import services.ConsumoAPI;
 import services.ConverterDados;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class DisplayImageInTerminal {
 
 	static ConverterDados converterDados = new ConverterDados();
 
+	static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) throws IOException, InterruptedException {
 
-		var listaCartas = ConsumoAPI.obterDados("https://deckofcardsapi.com/api/deck/4gtcpb1r8807/draw/?count=5");
+		System.out.println("Digite a quantidade de jogadores");
+		int numero_jogadores = scanner.nextInt();
+		scanner.nextLine();
+		List<String> jogadores = new ArrayList<>();
 
-		var listaCartasConvertida = converterDados.obterDados(listaCartas, Baralho.class);
+		for(int i =0;  i<numero_jogadores;i ++){
+			System.out.println("Digite o nome do jogador "+ i+": ");
+			String name = scanner.nextLine();
+			jogadores.add(name);
+		}
 
-		var teste = listaCartasConvertida.cards().stream().map(DisplayImageInTerminal::formatCard).collect(Collectors.toList());
+		System.out.println(jogadores);
 
-		teste.forEach(e-> System.out.print(e.cardFormat()));
+		jogadores.forEach(jogador->{
+			System.out.println("Gerando cartas para o jogador:" + jogador);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+			System.out.println("...carregando");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+
+			var listaCartas = ConsumoAPI.obterDados("https://deckofcardsapi.com/api/deck/omo5lwejh5va/draw/?count=2");
+
+			var listaCartasConvertida = converterDados.obterDados(listaCartas, Baralho.class);
+
+			var teste = listaCartasConvertida.cards().stream().map(DisplayImageInTerminal::formatCard).collect(Collectors.toList());
+
+			teste.forEach(e-> System.out.print(e.cardFormat()));
+
+			System.out.println(" \n Digite 1 para passar a vez para o proximo jogardo para que ele possa ver suas cartas tambem:");
+			var proximo =scanner.nextInt();
+
+			while (proximo!=1){
+				proximo =scanner.nextInt();
+			}
+		});
+
+
+
+
 	}
 
 	public static Carta formatCard(Carta carta) {
